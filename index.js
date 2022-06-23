7763c160148e56b323f7420fb26bfb1f6eea66de
Murilo Cavalcanti <murilo0300@hotmail.com.br>
2022-06-23 01:46:50 -0300

    Adicionado mudanças no JS

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-console.log('Hello World!');
+/*console.log('Hello World!');
+
+var x = false;
+console.log(x);
+console.log(typeof(x));*/
+
+let array = [1,2,'string',['array1']];
+//console.log(array[0]); //para mostrar o array, neste caso o de posição 0, que é o primeiro da lista//
+
+//forEach
+
+//array.forEach(function(item, index){console.log(item, index)});
+
+//push
+
+//array.push('item');
+//console.log(array);
+
+
+
